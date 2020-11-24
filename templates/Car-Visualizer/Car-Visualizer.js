8bfd6d4057dc301dff0d185209edd73d71162fc0
Javier Díaz <javidiazdom@gmail.com>
2020-11-24 12:25:06 +0000

    Car visualizer responsive y ajax

--- templates/Car-Visualizer/Car-Visualizer.js
+++ templates/Car-Visualizer/Car-Visualizer.js
@@ -7,10 +7,35 @@ $(document).ready(function () {
 
 function setData(response,status) {
     const content = response.contenido;
+    $(".containercar")[0].innerHTML = '';
 
     content.forEach((element, index) => {
-        $(".item")[index].childNodes[1].src = element.foto
-        console.log($(".item")[index].childNodes[3].childNodes[1].childNodes)
-        $(".item")[index].childNodes[3].childNodes[1].childNodes[0].innerHTML = element.nombre
+        var item = document.createElement("div");
+        item.className = "item";
+        var img = document.createElement("img");
+        img.src = element.foto;
+        item.appendChild(img);
+        var carName = document.createElement("div");
+        carName.className = "bordertext";
+        carName.appendChild(document.createElement("center"))
+        carName.childNodes[0].innerHTML = element.nombre;
+        carName.childNodes[0].className = "cartext";
+        item.appendChild(carName)
+        item.onclick = () => {
+            window.location.replace("");
+        }
+        var containerEsp = document.createElement("div");
+        containerEsp.className = "containeresp";
+        var tag = document.createElement("h2");
+        tag.innerHTML = element.nombre;
+        var category = document.createElement("h3");
+        category.innerHTML = element.categoria
+        var time = document.createElement("h4");
+        time.innerHTML = element.minutos
+        containerEsp.appendChild(tag)
+        containerEsp.appendChild(category)
+        containerEsp.appendChild(time)
+        $(".containercar")[0].appendChild(item);
+        $(".containercar")[0].appendChild(containerEsp)
     });
 }
